7626856909f95d96e0479090de0938981e8ca238
personabe1984 <personabe1984@gmail.com>
2018-08-26 19:59:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+
+
+/*
+ternaryCheckCity()
+— Use a ternary operator to return the correct response based on the desired destination of the passenger.
+switchOnCharmFromTip()
+— Use a switch statement to return a different response based on the generosity of the passenger's tip.
+*/
+
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return "This one is on me!";
+  }else if (distance > 2000 && distance <= 2500 ){
+    return "I will gladly take your thirty bucks.";
+  }else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+
+    default:
+      return "Bye.";
+  }
+}
