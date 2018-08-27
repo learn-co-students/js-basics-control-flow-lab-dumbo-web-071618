a9c0429618667c9d5186ccf1ec6d0f9e540bdfdd
rosatu <rosalina.tu@gmail.com>
2018-08-27 09:00:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code in this file!
+
+
+function scuberGreetingForFeet(num) {
+  if (num <= 400){
+    return 'This one is on me!';
+  }
+  else if (num > 2500){
+    return 'No can do.';
+  }
+  else if (num > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }
+ }
+
+function ternaryCheckCity(city){
+   return (city === "NYC" ? "Ok, sounds good." : "No go.");
+}
+
+let tip ;
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+       case 'generous':
+        return 'Thank you so much.';
+        break;
+       case 'not as generous':
+        return 'Thank you.';
+        break;
+       case 'thanks for everything':
+        return 'Bye.';
+        break;
+      }
+    }
